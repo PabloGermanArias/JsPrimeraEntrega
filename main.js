9b43bd32729a0633d1140e9b531fec2ebcbd9060
ariaspablogerman <rynor88@gmail.com>
2023-11-19 17:03:18 -0300

    segunda pre entrega

--- main.js
+++ main.js
@@ -1,24 +1,81 @@
 
-let alumnosPresentes = 0;
-let contador = 1;
+//* basicamente es la estructura de un control de productos en los que se incorpora un cartel de bienvenida
+//*con opciones a elegir.
+//* se pueden ver en consola los productos q hay, buscar productos, y de no encontrarlos agregarlos si asi lo desea
 
-do {
-  const nombreAlumno = prompt(`Ingrese el nombre del alumno ${contador} (o escriba "fin" para terminar):`);
-
-  if (nombreAlumno.toLowerCase() === "fin") {
-    break;
-  }
-
-  alumnosPresentes++;
-  contador++;
-} while (alumnosPresentes < 10);
-
-if (alumnosPresentes >= 10) {
-  alert("Todos los alumnos están presentes en el aula.");
-} else {
-  alert("No hay suficientes alumnos presentes en el aula.");
+const Producto = function(nombre,precio,stock){
+    this.nombre = nombre
+    this.precio = precio
+    this.stock = stock
 }
 
+let producto1 = new Producto("Paracetamol", 700, 20)
+let producto2 = new Producto("alikal", 650, 130)
+let producto3 = new Producto("ibuprofeno 600mg", 1200, 300)
 
+let listado = [producto1,producto2,producto3]
 
-/* basicamente seria un contador de alumnos minimos para poder iniciar una clase */ 
+function buscarProducto(){
+    let queBuscas = prompt("Ingresar el nombre del articulo que buscas").toUpperCase()
+    let resultado = listado.filter((x)=>x.nombre.toUpperCase().includes(queBuscas))
+
+    if(resultado.length >0){
+        console.table(resultado)
+    }
+    else{
+        alert ("no se encontro resultado de " + queBuscas)
+        let respuesta= confirm("Desea agregarlo al listado?")
+
+        if (respuesta == true){
+            crearProducto()
+        }     
+    }         
+} 
+
+function crearProducto(){
+    let nombre = prompt ("nombre del nuevo producto")
+    let precio = parseFloat(prompt("cual es el precio del producto?"))
+    let stock = parseInt (prompt("ingrese la cantidad de unidades a registrar"))
+
+    if(nombre === null || isNaN(stock)|| isNaN(precio)|| nombre == null){
+        alert("Datos invalidos")
+        let respuesta= confirm("desa volver a ingresarlos?")
+        if(respuesta== true){
+            return crearProducto()
+        }  
+    }
+    
+let producto = new Producto( nombre,precio, stock)
+    listado.push(producto)
+    console.table(listado) 
+   
+} 
+
+function mostrarMenu() {
+    alert("¡Bienvenido al sistema de gestión de productos!\n\nSeleccione una opción:\n\n1. Buscar Producto\n2. Agregar Producto\n3. Mostrar Listado");
+
+    let opcion = prompt("Ingrese el número de la opción que desea:");
+
+    switch (opcion) {
+        case "1":
+            buscarProducto();
+            break;
+        case "2":
+            crearProducto();
+            break;
+        case "3":
+            mostrarListado();
+            break;
+        default:
+            alert("Opción no válida. Por favor, elija 1, 2 o 3.");
+            mostrarMenu();
+            break;
+    }
+}
+
+function mostrarListado() {
+    console.table(listado);
+}
+
+mostrarMenu();
+    
