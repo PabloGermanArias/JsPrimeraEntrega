855a019e82a616fcf5b8abcb790711f2a722a50b
ariaspablogerman <rynor88@gmail.com>
2023-12-04 23:36:45 -0300

    3er entrega

--- main.js
+++ main.js
@@ -1,80 +1,75 @@
+// declaramos las variables en la busqueda de producto
 
-//* basicamente es la estructura de un control de productos en los que se incorpora un cartel de bienvenida
-//*con opciones a elegir.
-//* se pueden ver en consola los productos q hay, buscar productos, y de no encontrarlos agregarlos si asi lo desea
-
-const Producto = function(nombre,precio,stock){
+const Producto = function(nombre, precio, stock) {
     this.nombre = nombre
     this.precio = precio
     this.stock = stock
 }
 
 let producto1 = new Producto("Paracetamol", 700, 20)
-let producto2 = new Producto("alikal", 650, 130)
-let producto3 = new Producto("ibuprofeno 600mg", 1200, 300)
+let producto2 = new Producto("Alikal", 650, 130)
+let producto3 = new Producto("Ibuprofeno 600mg", 1200, 300)
 
-let listado = [producto1,producto2,producto3]
+let listado = [producto1, producto2, producto3]
 
-function buscarProducto(){
-    let queBuscas = prompt("Ingresar el nombre del articulo que buscas").toUpperCase()
-    let resultado = listado.filter((x)=>x.nombre.toUpperCase().includes(queBuscas))
+// Función para guardar la lista en localStorage
+function guardarListaEnLocalStorage() {
+    localStorage.setItem('productos', JSON.stringify(listado))
+}
 
-    if(resultado.length >0){
-        console.table(resultado)
-    }   else{
-        alert ("no se encontro resultado de " + queBuscas)
-        let respuesta= confirm("Desea agregarlo al listado?")
-
-        if (respuesta == true){
-            crearProducto()
-        }     
-    }         
-} 
-
-function crearProducto(){
-    let nombre = prompt ("nombre del nuevo producto")
-    let precio = parseFloat(prompt("cual es el precio del producto?"))
-    let stock = parseInt (prompt("ingrese la cantidad de unidades a registrar"))
-
-    if(nombre === null || isNaN(stock)|| isNaN(precio)|| nombre == null){
-        alert("Datos invalidos")
-        let respuesta= confirm("desa volver a ingresarlos?")
-        if(respuesta== true){
-            return crearProducto()
-        }  
-    }
-    
-let producto = new Producto( nombre,precio, stock)
-    listado.push(producto)
-    console.table(listado) 
-   
-} 
-
-function mostrarMenu() {
-    alert("¡Bienvenido al sistema de gestión de productos!\n\nSeleccione una opción:\n\n1. Buscar Producto\n2. Agregar Producto\n3. Mostrar Listado")
-
-    let opcion = prompt("Ingrese el número de la opción que desea:")
-
-    switch (opcion) {
-        case "1":
-            buscarProducto()
-            break
-        case "2":
-            crearProducto()
-            break
-        case "3":
-            mostrarListado()
-            break
-        default:
-            alert("Opción no válida. Por favor, elija 1, 2 o 3.")
-            mostrarMenu();
-            break
+// Función para cargar la lista desde localStorage al cargar la página
+function cargarListaDesdeLocalStorage() {
+    const storedListado = localStorage.getItem('productos')
+    if (storedListado) {
+        listado = JSON.parse(storedListado).map(item => new Producto(item.nombre, item.precio, item.stock))
     }
 }
 
-function mostrarListado() {
-    console.table(listado)
-}
+// Cargar lista desde localStorage al cargar la página
+cargarListaDesdeLocalStorage();
 
-mostrarMenu()
-    
+let valorIngresado = document.getElementById("input")
+let boton = document.getElementById("boton")
+let mostrarDato = document.getElementById("mostrarInput")
+
+boton.addEventListener("click", () => {
+    let dato = valorIngresado.value.toUpperCase();
+
+    let resultado = listado.filter((x) => x.nombre.toUpperCase().includes(dato))
+
+    if (resultado.length > 0) {
+        mostrarDato.innerHTML = resultado.map((x) => x.nombre).join(', ')
+    } else {
+        mostrarDato.innerHTML = "No se encontraron resultados"
+    }
+})
+// declaramos las variables para guardar productos
+
+let inputNombre = document.getElementById("inputNombre")
+let inputPrecio = document.getElementById("inputPrecio")
+let inputStock = document.getElementById("inputStock")
+let botonGuardar = document.getElementById("botonGuardar")
+let mostrardato2 = document.getElementById("mostrardato2")
+
+botonGuardar.addEventListener("click", () => {
+    let nuevoNombre = inputNombre.value;
+    let nuevoPrecio = parseFloat(inputPrecio.value);
+    let nuevoStock = parseInt(inputStock.value);
+
+    if (nuevoNombre && !isNaN(nuevoPrecio) && !isNaN(nuevoStock)) {
+        let nuevoProducto = new Producto(nuevoNombre, nuevoPrecio, nuevoStock)
+        listado.push(nuevoProducto)
+        inputNombre.value = ""
+        inputPrecio.value = ""
+        inputStock.value = ""
+        alert("Producto agregado correctamente")
+        mostrardato2.innerHTML = ""
+        mostrarDato.innerHTML = ""
+
+        // Guardar lista actualizada en localStorage
+        guardarListaEnLocalStorage()
+
+    } else {
+        mostrardato2.innerHTML = "Por favor, ingrese valores válidos para agregar un producto."
+    }
+})
